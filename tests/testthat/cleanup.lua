44c85400473741f07856099882d35e76853b6780
Konstantin Sorokin <kvs@sigterm.ru>
2016-11-04 14:58:42 +0300

    add more checks

--- tests/testthat/cleanup.lua
+++ tests/testthat/cleanup.lua
@@ -1,5 +1,12 @@
-box.space.test:drop()
-box.space.test2:drop()
+if box.space.test then
+    box.space.test:drop()
+end
 
-box.schema.user.revoke('guest', 'execute', 'function', 'add_two_numbers')
-box.schema.func.drop('add_two_numbers')
+if box.space.test2 then
+    box.space.test2:drop()
+end
+
+if box.schema.func.exists('add_two_numbers') then
+    box.schema.user.revoke('guest', 'execute', 'function', 'add_two_numbers')
+    box.schema.func.drop('add_two_numbers')
+end
